c6a59946e5f8c33b88f79ccd4e8e75facb2e84ba
mmumby <meredith.mumby@outlook.com>
2018-03-07 12:55:17 -0800

    updated PR

--- src/app/reusables/modals/user-modal/user-modal.component.ts
+++ src/app/reusables/modals/user-modal/user-modal.component.ts
@@ -1,15 +1,21 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'user-modal',
   templateUrl: './user-modal.component.html',
   styleUrls: ['./user-modal.component.scss']
 })
-export class UserModalComponent {
+export class UserModalComponent implements OnInit {
   @Input('modal') modal: any;
   @Input() closeValue: any;
+  @Output() submitValue: EventEmitter<any> = new EventEmitter();
 
   constructor() { }
+
+  onSubmit() {
+    this.submitValue.emit();
+  }
 
   close() {
     this.closeValue();
