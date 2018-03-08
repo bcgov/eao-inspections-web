50bc59e8d42832f1321cb1e337c43780372d543c
thehulk <samuel.yj.lee@gmail.com>
2018-03-08 11:31:49 -0800

    minor changes to fix typescript errors, and update to change names

--- src/app/reusables/modals/user-modal/user-modal.component.ts
+++ src/app/reusables/modals/user-modal/user-modal.component.ts
@@ -6,7 +6,7 @@ import { NgForm } from '@angular/forms';
   templateUrl: './user-modal.component.html',
   styleUrls: ['./user-modal.component.scss']
 })
-export class UserModalComponent implements OnInit {
+export class UserModalComponent {
   @Input('modal') modal: any;
   @Input() closeValue: any;
   @Output() submitValue: EventEmitter<any> = new EventEmitter();
