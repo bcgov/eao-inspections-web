4876fc7ce552bcae6ac12f745c1ce3ebc0dfb697
Terry Kang <terry@freshworks.io>
2018-03-09 11:22:42 -0800

    Create node server and build scripts for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  parseId: 'YZanozUMkq5PS1zhjx6BY0x837G08zY981625VNV',
+  parseKey: 'qBgOgH4iiWqxhlYsEiCMJAUmJ2Ngaqrl5FAFXig1a',
+  parseURL: 'https://parseapi.back4app.com/'
 };
