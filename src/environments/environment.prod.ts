d61be3d4a1075d3a422c2a584670c92417ee6f6b
thehulk <samuel.yj.lee@gmail.com>
2018-03-13 16:43:18 -0700

    updated changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   parseId: 'YZanozUMkq5PS1zhjx6BY0x837G08zY981625VNV',
   parseKey: 'qBgOgH4iiWqxhlYsEiCMJAUmJ2Ngaqrl5FAFXig1a',
-  parseURL: 'https://parseapi.back4app.com/'
+  parseURL: 'https://parseapi.back4app.com/',
+  parseMasterKey: '9S8jx2UmaQop3paPsv99sHfen2sQXQDlaU8f9IsL'
 };
