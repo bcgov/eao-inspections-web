c6153302a7a9ebbe4d7fc091f388942eef34ec95
Terry Kang <terry@freshworks.io>
2018-03-14 15:31:48 -0700

    Create report-view-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -46,6 +46,7 @@ import { ArchivedInspectionsComponent } from './dashboard/admin/reports-view/arc
 import { PermissionsModalComponent } from './reusables/modals/permissions-modal/permissions-modal.component';
 import { OrderByPipe } from './directives/orderby.pipe';
 import { AdminTeamCardComponent } from './dashboard/admin/teams-view/admin-team-card/admin-team-card.component';
+import { ReportViewListComponent } from './dashboard/admin/reports-view/report-view-list/report-view-list.component';
 
 @NgModule({
   declarations: [
@@ -82,7 +83,8 @@ import { AdminTeamCardComponent } from './dashboard/admin/teams-view/admin-team-
     ArchivedInspectionsComponent,
     PermissionsModalComponent,
     OrderByPipe,
-    AdminTeamCardComponent
+    AdminTeamCardComponent,
+    ReportViewListComponent 
   ],
   imports: [
     BrowserModule,
---
+++ src/app/dashboard/admin/reports-view/report-view-list/report-view-list.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { Inspection } from '../../../../../models/inspection.model';
+import { Team } from '../../../../../models/team.model';
+import { ReportService } from '../../../../../services/report.service';
+import { TeamService } from '../../../../../services/team.service';
+import { ActivatedRoute } from '@angular/router';
+import * as String from '../../../../../constants/strings';
+
+@Component({
+  selector: 'report-view-list',
+  templateUrl: './report-view-list.component.html',
+  styleUrls: ['./report-view-list.component.scss'],
+  providers: [ReportService, TeamService]
+})
+export class ReportViewListComponent implements OnInit {
+  emptyContent = {
+    image: '../../assets/team-lg.png',
+    message: String.EMPTY_INSPECTIONS,
+  };
+
+  isDesc: Boolean = false;
+  direction: number;
+  column: string;
+
+  team: Team;
+  data: Array<Inspection> = [];
+  fields: Array<any>;
+  actions: Array<any>;
+
+  constructor(private reportService: ReportService, private teamService: TeamService, private route: ActivatedRoute) {
+    this.fields = ['title', 'project', 'submitted', 'inspector', 'actions'];
+    this.actions = ['download', 'permission', 'archive'];
+  }
+
+  ngOnInit() {
+    this.sort('updatedAt');
+    const teamId = this.route.snapshot.params['id'];
+    this.teamService.getTeam(teamId).then((team) => {
+      this.team = team;
+    });
+    this.reportService.getTeamReports(teamId)
+      .then((results) => {
+        this.data = results;
+      });
+  }
+
+  setDefaultPic() {
+    this.team.image = '../../../assets/team-logo.png';
+  }
+
+  sort(property: string) {
+    this.isDesc = !this.isDesc;
+    this.column = property;
+    this.direction = this.isDesc ? 1 : -1;
+  }
+}
