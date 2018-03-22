1af2b6e50cd30ae543fb85e510a7d6428bbe72a1
mmumby <meredith.mumby@outlook.com>
2018-03-22 09:29:00 -0700

    added unit test for password modal

--- src/app/reusables/modals/password-modal/password-modal.component.spec.ts
+++ src/app/reusables/modals/password-modal/password-modal.component.spec.ts
@@ -1,14 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, DebugElement } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { PasswordModalComponent } from './password-modal.component';
 
 describe('PasswordModalComponent', () => {
   let component: PasswordModalComponent;
   let fixture: ComponentFixture<PasswordModalComponent>;
+  let mockData;
+  let mockModal;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PasswordModalComponent ]
+      declarations: [ PasswordModalComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+      imports: [FormsModule]
     })
     .compileComponents();
   }));
@@ -16,6 +22,11 @@ describe('PasswordModalComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PasswordModalComponent);
     component = fixture.componentInstance;
+
+    mockModal = { message: "Update Password" }
+    mockData = { password: "mockPassword"}
+    component.data = mockData;
+    component.modal = mockModal;
     fixture.detectChanges();
   });
 
