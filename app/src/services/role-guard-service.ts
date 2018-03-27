713af3af286687e11b7f95dce71563fe44fc0b37
mmumby <meredith.mumby@outlook.com>
2018-03-27 15:55:09 -0700

    changed role guard to only allow superadmin or admin to view

--- app/src/services/role-guard-service.ts
+++ app/src/services/role-guard-service.ts
@@ -1,23 +1,18 @@
 import { Injectable } from '@angular/core';
-import { Router, CanActivate, ActivatedRouteSnapshot} from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
 import {AuthService} from './auth.service';
+import * as Route from '../constants/routes';
 
 @Injectable()
 export class RoleGuardService implements CanActivate {
   constructor(public authService: AuthService, public router: Router) {}
-  canActivate(route: ActivatedRouteSnapshot): Promise<boolean> {
-    return new Promise((resolve, reject) => {
-      const expectedRole = route.data['expectedRole'];
-      // console.log(expectedRole);
-      this.authService.getRole(expectedRole).then((val) => {
-        // console.log(val);
-        if (val) {
-          resolve(true);
-        } else {
-          resolve(false);
-        }
-      });
-    });
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.authService.isAdmin() || this.authService.isSuperAdmin()) {
+      return true;
+    } else {
+      this.router.navigate([Route.HOME(false)]);
+    }
+    return false;
   }
 }
